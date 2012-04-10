bcbcf77906bd4d189ce9528c9757c5b8ddc07052
Rob Esris <robesris@gmail.com>
2012-04-10 01:14:04 -0400

    Began writing feature and steps for movement and capture.

---
+++ features/gameplay.feature
@@ -0,0 +1,18 @@
+Feature: Gameplay
+  As a player
+  In order to play the game
+  I want to make moves
+
+  Scenario: Capture a flipped Tro with a Black Stone
+    Given player 1 has a 'Tro' at 'c3'
+    And player 1s 'Tro' is flipped
+    And player 2 has a 'Black Stone' at 'c4'
+    And player 1 has 0 crystals
+    And player 2 has 0 crystals
+    And the graveyard is empty
+    And it is player 2s turn
+    When player 2 moves from 'c4' to 'c3'
+    Then player 1s 'Tro' should be in the graveyard
+    And players 2s 'Black Stone' should be at 'c4'
+    And player 1 should have 10 crystals
+    And player 2 should have 1 crystal
---
+++ features/step_definitions/gameplay_steps.rb
@@ -0,0 +1,56 @@
+Given /^an empty board$/ do
+  @game = Game.new
+  @player = [nil, { :pieces => {} }, { :pieces => {} }]
+end
+
+Given /^player (\d+) has a '(.*)' at '([a-g])(\d+)'$/ do |pnum, piece_name, row, col|
+  piece = Kernel.const_get(piece_name.humanize).new
+  piece.row = row
+  piece.col = col
+  piece.save
+  @game.player(pnum).pieces << piece
+  @player[pnum][:pieces][piece_name] = piece
+end
+
+Given /^player (\d+)s '(.*)' is flipped$/ do |pnum, piece_name|
+  piece = @player[pnum][:pieces][piece_name]
+  piece.flipped = true
+  piece.save
+end
+
+Given /^player (\d+) has (\d+) crystals$/ do |pnum, num|
+  player = @game.player(pnum)
+  player.crystals = num
+  player.save
+end
+
+Given /^the graveyard is empty$/ do
+  @game.graveyard.empty
+end
+
+Given /^it is player (\d+)s turn$/ do |pnum|
+  @game.active_player = pnum
+end
+
+When /^player (\d+) moves from '([a-g])(\d+)' to '([a-g])(\d+)'$/ do |pnum, row1, col1, row2, col2|
+  @game.move(row1, col1, row2, col2)
+end
+
+Then /^player (\d+)s '(.*)' should be in the graveyard$/ do |pnum, piece_name|
+  @player[pnum][pieces][piece_name].space.should == :graveyard
+  @player[pnum][pieces][piece_name].row.should == GRAVEYARD
+  @player[pnum][pieces][piece_name].col.should == GRAVEYARD
+end
+
+Then /^players (\d+)s 'Black Stone' should be at 'c(\d+)'$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^player (\d+) should have (\d+) crystals$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^player (\d+) should have (\d+) crystal$/ do |arg1, arg2|
+  pending # express the regexp above with the code you wish you had
+end
+
