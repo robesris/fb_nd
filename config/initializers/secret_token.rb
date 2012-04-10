d1b0f16b20cb20ae3a65d3f199071247d4eb69d3
Rob Esris <robesris@gmail.com>
2012-04-09 23:27:20 -0400

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FbNd::Application.config.secret_token = '8599fb6b89b42ce3f542e91cab6728a9074889e70e31bf1a4738c31da4bd6ee85a0c22d1e24ce42df77e964df96fb3d22153b0fadf93157ea189f050013facdf'
