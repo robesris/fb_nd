7d259a54623fd5c4eff812ede6cc2f6eec08dc1e
Rob Esris <robesris@gmail.com>
2012-05-24 00:45:41 -0400

    This one line that was commented out was causing all the js problems - apparently it doesn't ONLY matter if we're talking about the prod environment.

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails/all'
 
 if defined?(Bundler)
   # If you precompile assets before deploying to production, use this line
-  #Bundler.require(*Rails.groups(:assets => %w(development test cucumber)))
+  Bundler.require(*Rails.groups(:assets => %w(development test cucumber)))
   # If you want your assets lazily compiled in production, use this line
   # Bundler.require(:default, :assets, Rails.env)
 end
@@ -55,6 +55,6 @@ module FbNd
     config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
-    config.assets.version = '1.1'
+    config.assets.version = '1.0'
   end
 end
