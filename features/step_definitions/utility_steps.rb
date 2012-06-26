4c48f2c4b322ea5eea7e7166cc3ce16f9167fb30
Rob Esris <robesris@gmail.com>
2012-06-26 15:59:31 -0400

    Added step to give user control of browser.

--- features/step_definitions/utility_steps.rb
+++ features/step_definitions/utility_steps.rb
@@ -1,7 +1,6 @@
 Given /^I pause$/ do
-  puts "Pausing..."
   debugger
-  puts "Resuming."
+  puts "Pausing..."
 end
 
 When /^I wait briefly$/ do
@@ -12,4 +11,11 @@ When /^I wait (\d+) seconds?/ do |num|
   sleep num.to_i
 end
 
+Given /^I allow user input?/ do
+  puts "Allowing use of the browser..."
+  page.find_by_id('debug').set('stop')
+  while my_browser && page.find_by_id('debug')[:value] == 'stop' && opponent_browser && page.find_by_id('debug')[:value] == 'stop'
+    sleep 1 
+  end
+end
 
